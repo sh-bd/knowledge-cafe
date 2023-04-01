6021ae33b675716ded0770e379d91997d58eb3f7
sh-bd <shamim_ice1202@std.bauet.ac.bd>
2023-04-01 13:32:15 +0600

    made the name meaningful

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,40 +4,40 @@ import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 
 const Card = ({ card, addReadingTime, addBookMark }) => {
 
-    const { Blog_image, Author_image, Blog_title, Read_time, Publish_Date, Author_name, tags } = card;
+    const { thumbnail, profilePic, title, time, date, name, tags } = card;
 
     const [bookmarked, setBookmarked] = useState(false);
 
     const toggleBookmark = () => {
         setBookmarked(!bookmarked);
-        addBookMark(Blog_title);
+        addBookMark(title);
     }
 
     return (
         <div className='mb-3'>
             <div className="card">
-                <img src={Blog_image} className="card-img-top" alt="" />
+                <img src={thumbnail} className="card-img-top" alt="" />
                 <div className="card-body">
                     <div className="d-flex justify-content-between align-items-center">
                         <div className="d-flex align-items-center gap-1">
                             <div className="">
-                                <img src={Author_image} width="70px" height="70px" className="rounded-circle" alt="" />
+                                <img src={profilePic} width="70px" height="70px" className="rounded-circle" alt="" />
                             </div>
                             <div>
-                                <p className="mb-0 fw-bold fs-5">{Author_name}</p>
-                                <p>{Publish_Date}</p>
+                                <p className="mb-0 fw-bold fs-5">{name}</p>
+                                <p>{date}</p>
                             </div>
                         </div>
                         <div className="d-flex align-items-start">
-                            <p>{Read_time} min read</p>
+                            <p>{time} min read</p>
                             <button onClick={toggleBookmark} className="border-0 bg-body">
                                 <FontAwesomeIcon icon={faBookmark} color={bookmarked ? 'red' : 'black'} />
                             </button>
                         </div>
                     </div>
-                    <h5 className="card-title fs-2 my-2 fw-bold">{Blog_title}</h5>
+                    <h5 className="card-title fs-2 my-2 fw-bold">{title}</h5>
                     <p className="card-text text-secondary">{tags}</p>
-                    <a href="#" onClick={() => addReadingTime(Read_time)}  >
+                    <a href="#" onClick={() => addReadingTime(time)}  >
                         Mark as read
                     </a>
                 </div>
