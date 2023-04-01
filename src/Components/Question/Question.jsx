954cb9f70a5423910108e76bf5d18e29e538e2f4
sh-bd <shamim_ice1202@std.bauet.ac.bd>
2023-04-01 19:08:46 +0600

    fixed font weight

--- src/Components/Question/Question.jsx
+++ src/Components/Question/Question.jsx
@@ -12,7 +12,7 @@ const Question = () => {
                 <div className="accordion mt-4" id="accordionExample">
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingOne">
-                            <button className="accordion-button fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <button className="accordion-button fw-semibold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                 1. Props vs state
                             </button>
                         </h2>
@@ -24,7 +24,7 @@ const Question = () => {
                     </div>
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingTwo">
-                            <button className="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <button className="accordion-button collapsed fw-semibold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                                 2. How does useState work?
                             </button>
                         </h2>
@@ -38,7 +38,7 @@ const Question = () => {
                     </div>
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingThree">
-                            <button className="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <button className="accordion-button collapsed fw-semibold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                                 3. Purpose of useEffect other than fetching data.
                             </button>
                         </h2>
@@ -50,7 +50,7 @@ const Question = () => {
                     </div>
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingFour">
-                            <button className="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                            <button className="accordion-button collapsed fw-semibold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
                                 4. How Does React work?
                             </button>
                         </h2>
@@ -58,22 +58,22 @@ const Question = () => {
                             <div className="accordion-body">
                                 <span className='fw-semibold'>React</span> works on mainly 6 basic concepts- <br /><br />
 
-                                i. <span className='fw-semibold'>JSX</span>:
+                                i. <span className='fw-medium'>JSX</span>:
                                 It is a HTML-like markup inside a javascript file. Some of the attributes and properties are different than normal HTML. <br />
 
-                                ii. <span className='fw-semibold'>Components</span>:
+                                ii. <span className='fw-medium'>Components</span>:
                                 Every piece of UI is a components and these are the foundation to building a website. The name always starts with a capital letter. <br />
 
-                                iii. <span className='fw-semibold'>Props</span>:
+                                iii. <span className='fw-medium'>Props</span>:
                                 React components use props to communicate with other each other. <br />
 
-                                iv. <span className='fw-semibold'>useState</span>:
+                                iv. <span className='fw-medium'>useState</span>:
                                 useSate is a react hook that lets the developer add a state variable to your component. <br />
 
-                                v. <span className='fw-semibold'>Event handlers</span>:
+                                v. <span className='fw-medium'>Event handlers</span>:
                                 When an event handlers updates the state of a component, it triggers a re-render of the component and its children. <br />
 
-                                vi. <span className='fw-semibold'>useEffect</span>:
+                                vi. <span className='fw-medium'>useEffect</span>:
                                 The useEffect Hook allows you to perform side effects in your components. i.e. fetching data. <br />
                             </div>
                         </div>
